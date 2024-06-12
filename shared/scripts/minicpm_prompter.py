687fbc3cc11505fdc1b335f5ade652914989def2
mfriedman-pr <michael@peanutrobotics.com>
2024-06-11 19:25:53 -0700

    fixed typos in idefics_prompter made minicpm_prompter

--- shared/scripts/minicpm_prompter.py
+++ shared/scripts/minicpm_prompter.py
@@ -6,7 +6,7 @@ import datetime as dt
 from pathlib import Path
 from PIL import Image
 import torch
-from transformers import AutoModelForVision2Seq, AutoProcessor, BitsAndBytesConfig
+from transformers import AutoModel, AutoTokenizer
 
 
 def parse_arguments():
@@ -18,43 +18,30 @@ def get_timestamp():
     return dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 def initialize_model(model_id):
-    processor = AutoProcessor.from_pretrained(
+    model = AutoModel.from_pretrained(
         model_id, 
-        size={"longest_edge": 672, "shortest_edge": 672},
-        do_image_splitting=False
-        )
-    quantization_config = BitsAndBytesConfig(
-        load_in_4bit=True,
-        bnb_4bit_quant_type="nf4",
-        bnb_4bit_use_double_quant=True,
-        bnb_4bit_compute_dtype=torch.float16
-    )
-    model = AutoModelForVision2Seq.from_pretrained(
-        model_id,
-        trust_remote_code=True,
-        torch_dtype=torch.float16,
-        _attn_implementation="flash_attention_2",
-        quantization_config=quantization_config,
-    )
-    return processor, model
+        trust_remote_code=True, 
+        torch_dtype=torch.float16)
+    tokenizer = AutoTokenizer.from_pretrained(model_id, trust_remote_code=True)
+    return model, tokenizer
 
-def process_images(image_paths, processor, model, prompt, device):
+def process_images(image_paths, tokenizer, model, prompt):
     results = {}
     for image_path in image_paths:
         try:
-            image = Image.open(image_path)
-            messages = [
-                {"role": "user", "content": [{"type": "image"}, {"type": "text", "text": prompt}]}
-            ]
-            prompt_full = processor.apply_chat_template(messages, add_generation_prompt=True)
-            inputs = processor(text=prompt_full, images=[image], return_tensors="pt")
-            inputs = {k: v.to(device) for k, v in inputs.items()}
-            
-            generated_ids = model.generate(**inputs, max_new_tokens=500)
-            generated_texts = processor.batch_decode(generated_ids, skip_special_tokens=True)
+            image = Image.open(image_path).convert("RGB")
+            msgs = [{'role': 'user', 'content': prompt}]
 
-            results[image_path.name] = {"response": generated_texts[0]}
-            print(f"{image_path.name}: {generated_texts[0]}")
+            response = model.chat(
+                image=image,
+                msgs=msgs,
+                tokenizer=tokenizer,
+                sampling=True, # if sampling=False, beam_search will be used by default
+                temperature=0.7,
+                # system_prompt='' # pass system_prompt if needed
+            )
+            results[image_path.name] = {"response": response}
+            print(f"{image_path.name}: {response}")
         except Exception as e:
             print(f"Error processing {image_path}: {e}")
             results[image_path.name] = {"error": str(e)}
@@ -72,19 +59,20 @@ def adjust_file_permissions(output_file):
 def main():
     args = parse_arguments()
     device = "cuda:0"
-    model_id = "HuggingFaceM4/idefics2-8b-chatty"
+    model_id = "openbmb/MiniCPM-Llama3-V-2_5-int4"
     prompt = "Name the objects on the table from left to right."
-    processor, model = initialize_model(model_id)
-
+    model, tokenizer = initialize_model(model_id)
+    model.eval()
+    
     start_time = get_timestamp()
     path = Path(args.path)
     results = {}
     if path.exists():
         if path.is_dir():
             image_paths = sorted(list(path.glob('rgb_????.png')))
-            results = process_images(image_paths, processor, model, prompt, device)
+            results = process_images(image_paths, tokenizer, model, prompt)
         elif path.is_file() and re.match(r'rgb_\d{4}\.png', path.name):
-            results = process_images([path], processor, model, prompt, device)
+            results = process_images([path], tokenizer, model, prompt)
         else:
             print("The file or directory does not match the expected pattern.")
     else:
@@ -92,7 +80,7 @@ def main():
 
     end_time = get_timestamp()
     output_data = {"model_id": model_id, "prompt": prompt, "start_time": start_time, "end_time": end_time, "responses": results}
-    output_file = f"idefics2_responses_{dt.datetime.now().strftime('%Y%m%d_%H%M%S')}.json"
+    output_file = f"minicpm-llama3-2-5-int4_responses_{dt.datetime.now().strftime('%Y%m%d_%H%M%S')}.json"
     with open(output_file, 'w') as f:
         json.dump(output_data, f, indent=4)
     print(f"Output written to {output_file}")
